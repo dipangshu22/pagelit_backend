c82933ab254cfc005e5421ac78fccec1944be6cb
dipangshu22 <dipangshu22@gmail.com>
2026-02-22 13:49:53 +0530

    vercel

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://pagelit-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
